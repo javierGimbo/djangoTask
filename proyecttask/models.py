4956001338ea19adaf7cfa3b8f322ca324cd36e2
javierGimbo <146812089+javierGimbo@users.noreply.github.com>
2023-11-06 21:09:31 +0100

    soluciono el timezone y el html

--- proyecttask/models.py
+++ proyecttask/models.py
@@ -7,6 +7,7 @@ class Task(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField()
     completado = models.BooleanField(default=False)
+    published_date= models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return self.title
--- proyecttask/views.py
+++ proyecttask/views.py
@@ -3,7 +3,7 @@ from django.utils import timezone
 from django.shortcuts import render
 from .models import Task
 
-def task_list(request):
+def proyecttask_list(request):
 
     task = Task.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'proyecttask/proyecttask_list.html', {'task': task})   
